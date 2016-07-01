477a8ea794ad01b5ff49a441b3b8741ff0d2cd16
metawing <metawing30@gmail.com>
2016-07-01 16:17:13 +0530

    Changed settings file

--- activork_new/settings.py
+++ activork_new/settings.py
@@ -54,7 +54,8 @@ NOTIFICATIONS_USE_JSONFIELD=True
 MEDIA_ROOT = ''
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = ''
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
