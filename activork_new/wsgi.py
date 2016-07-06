cdbbad4820f667d0e78fa50490e7675317967f63
metawing <metawing30@gmail.com>
2016-07-07 05:02:51 +0530

    abc

--- activork_new/wsgi.py
+++ activork_new/wsgi.py
@@ -11,4 +11,14 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activork_new.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
+
+
+"""
+application = get_wsgi_application()"""
