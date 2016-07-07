a82b7824d46f36549cc23940f856e0bf2d54a852
metawing <metawing30@gmail.com>
2016-07-07 12:36:52 +0530

    ef

---
+++ myapp/templatetags/custom_tags.py
@@ -0,0 +1,29 @@
+from django import template
+register = template.Library()
+from entities.models import UserProfile
+import os
+
+def decode_choices(intent):
+    """
+    Decode intent choices
+    """
+    decoder = dict(UserProfile.MY_CHOICES)
+    decoded = [decoder[i] for i in intent]
+    decoded.sort()
+    return ', '.join(decoded)
+
+def improve_quality(path):
+	cmd = "pwd"
+	new_path = os.path.realpath("profile_photos")
+	print path
+	location = path.split("/")
+	print "location",location
+	actual_path = new_path + "/" + location[-1]
+	print actual_path
+	cmd = "convert -contrast -enhance -density 500 -normalize -quality 100 "+actual_path+" "+actual_path+""
+	os.system(cmd)
+	return path
+
+
+register.filter('decode_choices', decode_choices)
+register.filter('improve_quality',improve_quality)
---
+++ myapp/templatetags/trial.py
@@ -0,0 +1,4 @@
+import os
+
+cmd = "convert ../../profile_photos/msg_clint02.png sample_new.png"
+os.system(cmd)
