cc3cd8eb151a96505f3bddc0cd8310d547cc168d
metawing <metawing30@gmail.com>
2016-07-11 15:29:27 +0530

    api_changes

--- django_social_app/models.py
+++ django_social_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from rest_framework import serializers
-
+from django import forms
 
 
 class MyUser(AbstractUser):
